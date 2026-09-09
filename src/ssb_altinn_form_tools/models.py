9460f35099bdfd3b7171acd560277493094f4ea5
dbo <dbo@ssb.no>
2026-09-09 13:11:43 +0200

    formatting

--- src/ssb_altinn_form_tools/models.py
+++ src/ssb_altinn_form_tools/models.py
@@ -202,7 +202,9 @@ class FormReception(BaseModel):
     ident: str = Field(validation_alias="enhetsIdent")
     refnr: str = Field(validation_alias="altinnReferanse")
     dato_mottatt: datetime.datetime = Field(validation_alias="altinnTidspunktLevert")
-    status: Literal["Ferdig", "Under arbeid", "Ubehandlet"] = Field(default="Under arbeid")
+    status: Literal["Ferdig", "Under arbeid", "Ubehandlet"] = Field(
+        default="Under arbeid"
+    )
     kommentar: str
     aktiv: bool
 
